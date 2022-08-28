527794dbe60fb8497a7235c67cab62a00585288e
Dave <davebudah@gmail.com>
2022-08-28 16:46:08 +0200

    Theme toggle added

--- js/main.js
+++ js/main.js
@@ -87,4 +87,80 @@ toggleSidebar.addEventListener('click', function () {
             item.textContent = item.dataset.text;
         })
     }
-})
+});
+
+// Theme Toggling
+function styleSwitcherToggle() {
+ const styleSwitcher = document.querySelector('.js-style-switcher'),
+ styleSwitcherToggler = document.querySelector('.js-style-switcher-toggler');
+
+ styleSwitcherToggler.addEventListener('click', function() {
+    styleSwitcher.classList.toggle('open');
+    this.querySelector('i').classList.toggle("fa-cancel");
+    this.querySelector('i').classList.toggle("fa-cog");
+ });
+}
+styleSwitcherToggle();
+
+// Color Themes
+function themeColors() {
+    const colorStyle = document.querySelector('.js-color-style'),
+        themeColorsContainer = document.querySelector('.js-theme-colors');
+
+    themeColorsContainer.addEventListener('click', ({target}) => {
+        if(target.classList.contains('js-theme-color-item')) {
+            localStorage.setItem('color',target.getAttribute('data-js-theme-color'));
+            setColor();
+        }
+    });
+    function setColor() {
+        let path = colorStyle.getAttribute('href').split('/');
+        path = path.slice(0, path.length-1);
+        colorStyle.setAttribute('href',path.join('/') + '/' + localStorage.getItem('color') + '.css');
+        if(document.querySelector('.js-theme-color-item.active')){
+            document.querySelector('.js-theme-color-item.active').classList.remove('active');
+        }
+        document.querySelector('[data-js-theme-color= ' + localStorage.getItem('color') + ']').classList.add('active');
+    }
+    if (localStorage.getItem('color') !== null) {
+        setColor();
+    } else {
+       const defaultColor = colorStyle.getAttribute('href').split('/').pop().split('.').shift();
+       document.querySelector('[data-js-theme-color=' + defaultColor + ']').classList.add('active');
+    }
+}
+themeColors();
+
+// Light and dark toggle
+
+function themeLightDark() {
+    const darkModeCheckBox = document.querySelector('.js-dark-mode');
+    const label = document.querySelector('label');
+
+    darkModeCheckBox.addEventListener('click', function() {
+        if(this.checked) {
+            localStorage.setItem('theme-dark', 'true');
+            label.textContent = 'Lite Mode';
+        } else {
+            localStorage.setItem('theme-dark', 'false');
+            label.textContent = 'Dark Mode';
+        }
+        themeMode();
+    });
+
+    function themeMode() {
+        if(localStorage.getItem('theme-dark') === 'false') {
+            document.body.classList.remove('t-dark');
+        } else {
+            document.body.classList.add('t-dark');
+        }
+    }
+    if(localStorage.getItem('theme-dark') !== null) {
+        themeMode();
+    }
+    if(document.body.classList.contains('t-dark')) {
+        darkModeCheckBox.checked = true;
+    }
+}
+themeLightDark();
+
